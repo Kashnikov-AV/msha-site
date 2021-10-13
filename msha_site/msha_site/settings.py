e68ac71f4dc62a6a0d365753dbe4e0bb942239a9
aleks <duo1245@yandex.ru>
2021-10-14 00:49:54 +0300

    issue databases

--- msha_site/msha_site/settings.py
+++ msha_site/msha_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3lf1^13z=*e(nw384#zh8u^gccko44!f=_)#)u7kh!7h*+dhzp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
